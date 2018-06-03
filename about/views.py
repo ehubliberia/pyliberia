b737eacd56ea83f1d8f36ae91793f45d430c1415
francisfmassaquoijr <francisfmassaquoijr@gmail.com>
2018-06-03 17:30:50 +0000

    Python Liberia latest update

--- about/views.py
+++ about/views.py
@@ -3,6 +3,9 @@ from .models import about
 
 def about_post(request):
 	post = about.objects.all()
-	context = {'post':post}
+	coc_lst = coc.objects.all()
+	constit = const.objects.all()
+	proj = project.objects.all()
+	context = {'post':post, 'proj':proj, 'coc_lst':coc_lst, 'constit':constit}
 	return render(request, 'index.html', context)
 
