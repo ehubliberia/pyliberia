ba6e201f6836a9aeae941d4850010ac5881afe2b
francisfmassaquoijr <francisfmassaquoijr@gmail.com>
2018-06-03 15:00:31 +0000

    Python Liberia latest update

--- pythonliberia/settings.py
+++ pythonliberia/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'projects', 
     'about', 
     'coc', 
-    'constitution', 
+    'constitution',
+     'whitenoise.runserver_nostatic', # new! 
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # new!
 ]
 
 ROOT_URLCONF = 'pythonliberia.urls'
@@ -119,8 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new!
